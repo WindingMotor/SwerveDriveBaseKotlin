87b9a80b45cf274a5d664d803d89734d6e7cc74e
WindingMotor <kid.subudhi@gmail.com>
2023-06-04 21:36:22 -0400

    Extensive Commenting

--- src/main/kotlin/frc/robot/wmlib/SwerveModule.kt
+++ src/main/kotlin/frc/robot/wmlib/SwerveModule.kt
@@ -24,21 +24,24 @@ class SwerveModule(
     private val absoluteEncoderOffsetRadians: Double,
     private val moduleName: String
 ){
-
-    // Object creation
+    // Create both motors using class from wmlib
     private val driveMotor: SparkMax = SparkMax(driveID, driveInverted, false, Constants.MK4SDS.DRIVE_ROT_2_METER, Constants.MK4SDS.DRIVE_RPM_2_MPS)
     private val turnMotor: SparkMax = SparkMax(turnID, turnInverted, false, Constants.MK4SDS.TURN_ROT_2_RAD, Constants.MK4SDS.TURN_RPM_2_RADPS)
+
+    // Create absolute encoder using class from wmlib
     private val absoluteEncoder: AbsoluteEncoder = AbsoluteEncoder(absoluteEncoderID, absoluteEncoderOffsetRadians)
 
+    // Create the turnPID with (PID) values from constants
     private val turnPID: PIDController = PIDController(Constants.SwerveConstants.TURN_MODULE_PID_P, Constants.SwerveConstants.TURN_MODULE_PID_I,
         Constants.SwerveConstants.TURN_MODULE_PID_D).apply { enableContinuousInput(-Math.PI, Math.PI) }
-    // ------------ //
 
-    // Functions
+    // Reset both encoders, drive to 0 and turn to absolute value
     fun resetEncoders(){ driveMotor.resetEncoder(); turnMotor.encoder.position = absoluteEncoder.getRadians() }
 
+    // @return (SwerveModuleState) Returns current state of the module
     fun getState(): SwerveModuleState = SwerveModuleState(driveMotor.getEncoderVelocity(), Rotation2d(turnMotor.getEncoderPosition()))
 
+    // @param (state) (SwerveModuleState) Set the current state of the module
     fun setState(state: SwerveModuleState){ if(Math.abs(state.speedMetersPerSecond) > Constants.SwerveConstants.TELEOP_MIN_STATE_CHANGE){
             var optimizedState = SwerveModuleState.optimize(state, getState().angle)
             driveMotor.set(optimizedState.speedMetersPerSecond / Constants.MK4SDS.THEORETICAL_MAX_SPEED)
@@ -46,24 +49,31 @@ class SwerveModule(
         }else{ stopMotors() }
     }
 
+    // Stop both module motors
     fun stopMotors(){ driveMotor.stop(); turnMotor.stop(); }
 
+    // @param (angle) (Rotation2d) Set the angle of the swerve module
     fun setAngle(angle: Rotation2d){ turnMotor.set(turnPID.calculate(turnMotor.encoder.position, angle.radians)) }
 
+    // @return (Double:ARRAY) An array of the motor currents, driveMotor then turnMotor order.
     fun getMotorsCurrent(): Array<Double> = arrayOf(driveMotor.motor.outputCurrent, turnMotor.motor.outputCurrent)
 
+    // @return (Double:ARRAY) An array of the motor temperatures, driveMotor then turnMotor order.
     fun getMotorsTemperature(): Array<Double> = arrayOf(driveMotor.motor.motorTemperature, turnMotor.motor.motorTemperature)
 
+    // @return (Short:ARRAY) An array of the motor faults, driveMotor then turnMotor order.
     fun getMotorFaults(): Array<Short> = arrayOf(driveMotor.motor.faults, turnMotor.motor.faults)
     
+    // @return (SwerveModulePosition) The current position of the swerve module 
     fun getPosition(): SwerveModulePosition = SwerveModulePosition(driveMotor.encoder.position, Rotation2d(turnMotor.encoder.position))
-    // ------------ //
 
-    // Smart Dashboard
+
+    // Print SmartDashboard data for this class
     fun updateSmartDashboard(){
         SmartDashboard.putString("Swerve module $moduleName :", getState().toString()) 
     }
 
+    // Print SmartDashboard debug data for this class
     fun updateSmartDashboardDebug(){ updateSmartDashboard()
         DriverStation.reportWarning("DEBUG is on for $moduleName", true)
         SmartDashboard.putNumber("${moduleName} ABE Raw: ", absoluteEncoder.encoder.absolutePosition)
--- src/main/kotlin/frc/robot/wmlib/SwerveSubsystem.kt
+++ src/main/kotlin/frc/robot/wmlib/SwerveSubsystem.kt
@@ -13,13 +13,15 @@ import frc.robot.Constants
 
 class SwerveSubsystem : SubsystemBase() {
 
-   // Object creation
+    // Create a navX gyro from wmlib
     val gyro = NavX()
 
+    // Create robot odometry
     val odometry = SwerveDriveOdometry(Constants.SwerveConstants.DRIVE_KINEMATICS,
     gyro.getRotation2d(), // Should be wrapped from -180 to 180 or 0 to 360 -> wmlib NavX class does this
     getAllPositions())
     
+    // Create 4 swerve modules with their respective constants
     private val frontLeft = SwerveModule(
         Constants.SwerveConstants.FRONT_LEFT_DRIVE_ID, Constants.SwerveConstants.FRONT_LEFT_TURN_ID,
         Constants.SwerveConstants.FRONT_LEFT_DRIVE_INVERTED, Constants.SwerveConstants.FRONT_LEFT_TURN_INVERTED,
@@ -41,49 +43,58 @@ class SwerveSubsystem : SubsystemBase() {
         Constants.SwerveConstants.BACK_RIGHT_ABE_ID, Constants.SwerveConstants.BACK_RIGHT_ABE_OFFSET_RAD, "Back Right");
     // ------------ //
 
-    // Swerve Modules
+    // Stop every module's motors
     fun stopAll(){ frontLeft.stopMotors(); frontRight.stopMotors(); backLeft.stopMotors(); backRight.stopMotors(); }
 
+    // Reset every module's encoders
     fun resetAllEncoders(){ frontLeft.resetEncoders(); frontRight.resetEncoders(); backLeft.resetEncoders(); backRight.resetEncoders(); }
 
+    // @param (angle) (Rotation2d) Set every swerve module to a set angle
     fun setAllAngles(angle: Rotation2d){ frontLeft.setAngle(angle); frontRight.setAngle(angle); backLeft.setAngle(angle); backRight.setAngle(angle); }
 
+    // @return (Short:ARRAY:ARRAY) A 2d array of the motor faults of each module 
     fun getAllFaults(): Array<Array<Short>> = arrayOf(frontLeft.getMotorFaults(), frontRight.getMotorFaults(), backLeft.getMotorFaults(), backRight.getMotorFaults())
 
+    // @return (SwerveModulePositon:ARRAY) The current position of each swerve module
     fun getAllPositions(): Array<SwerveModulePosition> = arrayOf(frontLeft.getPosition(), frontRight.getPosition(), backLeft.getPosition(), backRight.getPosition())
 
+    // @param (states) (SwerveModuleState:ARRAY) The new state to set each swerve module to
     fun setStates(states: Array<SwerveModuleState>) {
         SwerveDriveKinematics.desaturateWheelSpeeds(states, Constants.MK4SDS.THEORETICAL_MAX_SPEED)
         frontLeft.setState(states[0]); frontRight.setState(states[1]);
         backLeft.setState(states[2]); backRight.setState(states[3]);
     }
-    // ------------ //
 
-    // Odometry
+    // @param (pose) (Pose2d) - The new pose to assign
     fun resetOdometry(pose: Pose2d){odometry.resetPosition(gyro.getRotation2d(), getAllPositions(), pose)}
-
+    
+    /* 
+    @param (pose) (Pose2d) The new pose to assign
+    @param (angle) (Rotation2d) The new angle to assign
+    */
     fun resetOdometry(pose: Pose2d, angle: Rotation2d){odometry.resetPosition(angle, getAllPositions(), pose)}
 
+    // @return (Pose2d) The current robot odometry pose
     fun getPoseMeters(): Pose2d = odometry.poseMeters
-    // ------------ //
 
-    // Loops
+    // Updates the robot odometry
     override fun periodic(){ odometry.update(gyro.getRotation2d(), getAllPositions()) }
 
+    // Print SmartDashboard data for this class
     private fun updateSmartDashboard(){ SmartDashboard.putString("Odometry Pose2d", getPoseMeters().toString()) }
 
+    // Print all SmartDashboard data for all nested classes
     fun updateAllSmartDashboard(){
         this.updateSmartDashboard(); gyro.updateSmartDashboard();
         frontLeft.updateSmartDashboard(); frontRight.updateSmartDashboard();
         backLeft.updateSmartDashboard(); backRight.updateSmartDashboard();
     }
 
+     // Print all SmartDashboard debug data for all nested classes
     fun updateAllSmartDashboardDebug(){
         this.updateSmartDashboard(); gyro.updateSmartDashboardDebug();
         frontLeft.updateSmartDashboardDebug(); frontRight.updateSmartDashboardDebug();
         backLeft.updateSmartDashboardDebug(); backRight.updateSmartDashboardDebug();
     }
-    // ------------ //
-
 
 }
